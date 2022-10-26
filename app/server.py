786e01cfcb53a7bdafcff942b0fb7b1df0e9fdb3
mulp00 <82061632+mulp00@users.noreply.github.com>
2022-10-26 11:21:09 +0200

    Update server.py
    oprava kodovani na utf8

--- app/server.py
+++ app/server.py
@@ -4,18 +4,18 @@ import psycopg2
 import logging
 
 conn = psycopg2.connect(
-    host="localhost",
+    host="db",
     database="srealitydb",
     user="postgres",
     password="root")
 
-hostName = "localhost"
+hostName = "0.0.0.0"
 serverPort = 8080
 
-class MyServer(BaseHTTPRequestHandler):
+class WebServer(BaseHTTPRequestHandler):
     def do_GET(self):
         cursor = conn.cursor()
-        postgreSQL_select_Query = "select title, image from properties"
+        postgreSQL_select_Query = "SELECT title, image FROM properties"
 
         cursor.execute(postgreSQL_select_Query)
         sproperties = cursor.fetchall()
@@ -23,10 +23,12 @@ class MyServer(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(bytes("<html><head><title>Demo server</title></head>", "utf-8"))
+        self.wfile.write(bytes(
+            "<html><head><meta content='text/html;charset=utf-8' http-equiv='Content-Type'><meta content='utf-8' http-equiv='encoding'><title>Sreality scrape web</title></head>",
+            "utf-8"))
         self.wfile.write(bytes("<body>", "utf-8"))
 
         for row in sproperties:
-            self.wfile.write(bytes("title: " + row[0] + " <img src='" + row[1] + "'>", "utf-8"))
+            self.wfile.write(bytes(row[0] + " <img src='" + row[1] + "'><br>", "utf-8"))
 
         self.wfile.write(bytes("</body>", "utf-8"))
